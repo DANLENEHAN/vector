fd4fb27f50ad200e6fcbfc3f218c005d30053ad5
Dan <daniel.lenehan.work@gmail.com>
2023-12-12 15:08:33 +0000

    Changed Login Name

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import LoginScreen from './src/screens/LoginScreen';
+import LoginScreen from './src/screens/Login';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
