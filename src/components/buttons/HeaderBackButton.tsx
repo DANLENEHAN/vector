372cf307a437c344d01b687f14ac1577b5321e53
Daniel Maguire <dmaguire1996@gmail.com>
2023-12-14 16:37:57 +0000

    Fixed stylesheet

--- src/components/buttons/HeaderBackButton.tsx
+++ src/components/buttons/HeaderBackButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
+import {TouchableOpacity, StyleSheet} from 'react-native';
 // Navigation
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../navigation/types';
@@ -8,29 +8,31 @@ import Icon from 'react-native-vector-icons/FontAwesome6';
 import {useTheme} from '../../context/ThemeContext';
 import {lightTheme, darkTheme} from '../../theme';
 
-
 type HeaderBackButtonProps = {
-    navigation: NativeStackNavigationProp<RootStackParamList>;
-
+  navigation: NativeStackNavigationProp<RootStackParamList>;
 };
 
 const HeaderBackButton: React.FC<HeaderBackButtonProps> = ({navigation}) => {
-//Setup theme for the component
+  //Setup theme for the component
   const {theme} = useTheme();
   const currentTheme = theme === 'dark' ? darkTheme : lightTheme;
   return (
-    <TouchableOpacity style={[styles.headerBackButton, {backgroundColor: currentTheme.background}]} onPress={() => navigation.navigate("Settings")}>
-        <Icon name="arrow-left" size={25} color={currentTheme.text}/>
+    <TouchableOpacity
+      style={[
+        styles.headerBackButton,
+        {backgroundColor: currentTheme.background},
+      ]}
+      onPress={() => navigation.navigate('Settings')}>
+      <Icon name="arrow-left" size={25} color={currentTheme.text} />
     </TouchableOpacity>
   );
 };
 
-const styles = {
-    headerBackButton: {
-        marginLeft: 10,
-        padding: 10,
-
-    }
-};
+const styles = StyleSheet.create({
+  headerBackButton: {
+    marginLeft: 10,
+    padding: 10,
+  },
+});
 
 export default HeaderBackButton;
