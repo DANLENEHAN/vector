c2b7dc1ee293a6bb55757b95d1e28fb249bcef14
Dan <daniel.lenehan.work@gmail.com>
2023-12-14 11:15:39 +0000

    Bottom navbar implementation

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
+
+// Navigation
 import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {RootStackParamList} from './types';
+import {RootStackParamList} from '../navigation/types';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 // Screens
 import LoginScreen from '../screens/Login';
 import HomeScreen from '../screens/Home';
 import SettingsScreen from '../screens/Settings';
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+import BottomNavBar from '../components/navbar/BottomNavBar';
 
-const Navigation: React.FC = () => {
+const Stack = createNativeStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator();
+
+type AppNavigatorProps = {
+  navigation: NativeStackNavigationProp<RootStackParamList>;
+};
+
+const AppNavigator: React.FC<AppNavigatorProps> = ({navigation}) => {
+  return (
+    // eslint-disable-next-line react/no-unstable-nested-components
+    <Tab.Navigator tabBar={() => <BottomNavBar navigation={navigation} />}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{headerShown: false}}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const AuthNavigator: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -19,19 +48,10 @@ const Navigation: React.FC = () => {
           component={LoginScreen}
           options={{headerShown: false}}
         />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Settings"
-          component={SettingsScreen}
-          options={{headerShown: false}}
-        />
+        <Stack.Screen name="App" component={AppNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-export default Navigation;
+export default AuthNavigator;
