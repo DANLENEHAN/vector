18342312dc9173793b681afa02d8498bef96169d
Daniel Maguire <dmaguire1996@gmail.com>
2024-01-09 16:37:08 +0000

    Fixed graph

--- src/screens/progress/WeightProgress.tsx
+++ src/screens/progress/WeightProgress.tsx
@@ -1,6 +1,6 @@
 // React imports
 import React, {useState} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {useFocusEffect} from '@react-navigation/native'; // Import useFocusEffect
 // Layouts
 import ScreenWrapper from '../../components/layout/ScreenWrapper';
@@ -21,13 +21,50 @@ import {getUserStats} from '../../services/api/blueprints/stat/functions';
 import {useTheme} from '../../context/ThemeContext';
 // Utils
 import {convertStats} from '../../utils/conversion';
-import {parse} from 'date-fns';
+import {parse, format} from 'date-fns';
 // Types
 import {ScreenProps} from '../types';
 import {StatType, WeightUnit} from '../../services/api/swagger/data-contracts';
 
+interface graphDataPoint {
+  date: number;
+  value: number;
+}
+
+class GraphPlotData {
+  data: graphDataPoint[];
+  unit: string;
+  averageValue: number;
+  averageLabel: string;
+
+  getAverageValue() {
+    let sum = 0;
+    let count = 0;
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].value != null) {
+        sum += this.data[i].value;
+        count++;
+      }
+    }
+    return sum / count;
+  }
+
+  getAverageLabel() {
+    const firstDate = this.data[0].date;
+    const lastDate = this.data[this.data.length - 1].date;
+    return `${format(firstDate, 'd MMM')} - ${format(lastDate, 'd MMM yyyy')}`;
+  }
+
+  constructor(data: graphDataPoint[], unit: string) {
+    this.unit = unit;
+    this.data = data;
+    this.averageValue = this.getAverageValue();
+    this.averageLabel = this.getAverageLabel();
+  }
+}
+
 const WeightProgress: React.FC<ScreenProps> = ({navigation}) => {
-  const [data, setData] = useState<any>([]);
+  const [data, setData] = useState<any>({});
   const {theme} = useTheme();
 
   const dateOptions = ['D', 'W', 'M', '6M', 'Y'];
@@ -54,35 +91,98 @@ const WeightProgress: React.FC<ScreenProps> = ({navigation}) => {
           //   date: new Date(d.created_at).valueOf(),
           //   value: d.value,
           // })) as graphData[];
-          const tempData = [
-            {date: parse('2023-01-01', 'yyyy-MM-dd', new Date()), value: 70},
-            {date: parse('2023-01-02', 'yyyy-MM-dd', new Date()), value: 71},
-            {date: parse('2023-01-03', 'yyyy-MM-dd', new Date()), value: 70},
-            {date: parse('2023-01-04', 'yyyy-MM-dd', new Date()), value: null},
-            {date: parse('2023-01-05', 'yyyy-MM-dd', new Date()), value: 70},
-            {date: parse('2023-01-06', 'yyyy-MM-dd', new Date()), value: 65},
-            {date: parse('2023-01-07', 'yyyy-MM-dd', new Date()), value: 64},
-            {date: parse('2023-01-08', 'yyyy-MM-dd', new Date()), value: null},
-            {date: parse('2023-01-09', 'yyyy-MM-dd', new Date()), value: null},
-            {date: parse('2023-01-10', 'yyyy-MM-dd', new Date()), value: null},
-            {date: parse('2023-01-11', 'yyyy-MM-dd', new Date()), value: null},
-            {date: parse('2023-01-12', 'yyyy-MM-dd', new Date()), value: null},
-            {date: parse('2023-01-13', 'yyyy-MM-dd', new Date()), value: null},
-            {date: parse('2023-01-14', 'yyyy-MM-dd', new Date()), value: 80},
-          ];
-
-          const graphData = {
-            D: [
-              {date: parse('2023-01-01', 'yyyy-MM-dd', new Date()), value: 70},
-              {date: parse('2023-01-02', 'yyyy-MM-dd', new Date()), value: 71},
-              {date: parse('2023-01-03', 'yyyy-MM-dd', new Date()), value: 70},
+          const tempData = new GraphPlotData(
+            [
               {
-                date: parse('2023-01-04', 'yyyy-MM-dd', new Date()),
+                date: parse('2023-01-01', 'yyyy-MM-dd', new Date()).valueOf(),
+                value: 70,
+              },
+              {
+                date: parse('2023-01-02', 'yyyy-MM-dd', new Date()).valueOf(),
+                value: 71,
+              },
+              {
+                date: parse('2023-01-03', 'yyyy-MM-dd', new Date()).valueOf(),
+                value: 70,
+              },
+              {
+                date: parse('2023-01-04', 'yyyy-MM-dd', new Date()).valueOf(),
                 value: null,
               },
-              {date: parse('2023-01-05', 'yyyy-MM-dd', new Date()), value: 70},
-              {date: parse('2023-01-06', 'yyyy-MM-dd', new Date()), value: 65},
+              {
+                date: parse('2023-01-05', 'yyyy-MM-dd', new Date()).valueOf(),
+                value: 70,
+              },
+              {
+                date: parse('2023-01-06', 'yyyy-MM-dd', new Date()).valueOf(),
+                value: 65,
+              },
+              {
+                date: parse('2023-01-07', 'yyyy-MM-dd', new Date()).valueOf(),
+                value: 64,
+              },
+              {
+                date: parse('2023-01-08', 'yyyy-MM-dd', new Date()).valueOf(),
+                value: null,
+              },
+              {
+                date: parse('2023-01-09', 'yyyy-MM-dd', new Date()).valueOf(),
+                value: null,
+              },
+              {
+                date: parse('2023-01-10', 'yyyy-MM-dd', new Date()).valueOf(),
+                value: null,
+              },
+              {
+                date: parse('2023-01-11', 'yyyy-MM-dd', new Date()).valueOf(),
+                value: null,
+              },
+              {
+                date: parse('2023-01-12', 'yyyy-MM-dd', new Date()).valueOf(),
+                value: null,
+              },
+              {
+                date: parse('2023-01-13', 'yyyy-MM-dd', new Date()).valueOf(),
+                value: null,
+              },
+              {
+                date: parse('2023-01-14', 'yyyy-MM-dd', new Date()).valueOf(),
+                value: 80,
+              },
             ],
+            weightUnitPref,
+          );
+
+          const graphData = {
+            D: new GraphPlotData(
+              [
+                {
+                  date: parse('2023-01-01', 'yyyy-MM-dd', new Date()).valueOf(),
+                  value: 70,
+                },
+                {
+                  date: parse('2023-01-02', 'yyyy-MM-dd', new Date()).valueOf(),
+                  value: 71,
+                },
+                {
+                  date: parse('2023-01-03', 'yyyy-MM-dd', new Date()).valueOf(),
+                  value: 70,
+                },
+                {
+                  date: parse('2023-01-04', 'yyyy-MM-dd', new Date()).valueOf(),
+                  value: 50,
+                },
+                {
+                  date: parse('2023-01-05', 'yyyy-MM-dd', new Date()).valueOf(),
+                  value: 70,
+                },
+                {
+                  date: parse('2023-01-06', 'yyyy-MM-dd', new Date()).valueOf(),
+                  value: 65,
+                },
+              ],
+              weightUnitPref,
+            ),
             W: tempData,
             M: tempData,
             '6M': tempData,
@@ -92,10 +192,7 @@ const WeightProgress: React.FC<ScreenProps> = ({navigation}) => {
         }
       };
       getUserWeights();
-      return () => {
-        // This is where you can return a function to be called when the component is unfocused or unmounted
-        // For example, you might want to clear some state or unsubscribe from a service.
-      };
+      return () => {};
     }, []),
   );
 
@@ -109,41 +206,6 @@ const WeightProgress: React.FC<ScreenProps> = ({navigation}) => {
       />
 
       <View style={styles.content}>
-        <View style={styles.averageWeightContainer}>
-          <Text
-            style={[
-              styles.averageWeightLabel,
-              {color: currentTheme.lightText, marginBottom: margins.xSmall},
-            ]}>
-            Average:
-          </Text>
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              marginBottom: margins.xSmall,
-            }}>
-            <Text
-              style={[styles.averageWeightValue, {color: currentTheme.text}]}>
-              87.65
-            </Text>
-            <Text
-              style={[
-                styles.averageWeightLabel,
-                {color: currentTheme.lightText, fontSize: fontSizes.large},
-              ]}>
-              {' '}
-              Kg
-            </Text>
-          </View>
-          <Text
-            style={[
-              styles.averageWeightLabel,
-              {color: currentTheme.lightText},
-            ]}>
-            1 Jan - 6 Jan 2024
-          </Text>
-        </View>
         <UnitSelector
           units={Object.values(dateOptions)}
           activeUnit={activePeriod}
@@ -153,14 +215,20 @@ const WeightProgress: React.FC<ScreenProps> = ({navigation}) => {
             width: '90%',
             color: currentTheme.text,
             marginTop: margins.xSmall,
-            marginBottom: margins.xSmall,
+            marginBottom: margins.medium,
           }}
         />
+
         <View style={styles.chartContainer}>
           <View
             style={{flex: 1, alignContent: 'center', justifyContent: 'center'}}>
             {data && activePeriod && data[activePeriod] && (
-              <LineGraph data={data[activePeriod]} />
+              <LineGraph
+                data={data[activePeriod].data}
+                averageLabel={data[activePeriod].averageLabel}
+                averageValue={data[activePeriod].averageValue}
+                unit={data[activePeriod].unit}
+              />
             )}
           </View>
         </View>
@@ -179,7 +247,7 @@ const styles = StyleSheet.create({
   },
   chartContainer: {
     width: '100%',
-    height: 300,
+    height: 250,
   },
   averageWeightContainer: {
     width: '90%',
