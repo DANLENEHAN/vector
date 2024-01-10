2ec6071b1dd11b31997ecec1f6c654b96aa9d329
Daniel Maguire <dmaguire1996@gmail.com>
2024-01-10 16:03:42 +0000

    Broke out and documented Linegraph component

---
+++ src/components/graphs/Line/AverageValue.tsx
@@ -0,0 +1,118 @@
+// React Import
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+// Theme
+import {
+  darkThemeColors,
+  lightThemeColors,
+  margins,
+  fontSizes,
+  fontWeights,
+} from '../../../styles/main';
+import {useSystem} from '../../../context/SystemContext';
+// Components
+import {AnimatedText} from '../../inputs/AnimatedText';
+// Typing
+import {type SharedValue} from 'react-native-reanimated';
+
+interface AverageValueText {
+  unit: string; // The unit for the average value
+  currentValue: SharedValue<string>; // The current value of the graph
+  currentDate: SharedValue<string>; // The current date of the graph
+}
+
+export const AverageValueText: React.FC<AverageValueText> = ({
+  currentValue,
+  currentDate,
+  unit,
+}) => {
+  // Setup theme for the component
+  const {theme} = useSystem();
+  const currentTheme = theme === 'dark' ? darkThemeColors : lightThemeColors;
+
+  // Render the component
+  /**
+   * Returns AverageValueText component with the following props:
+   * Component:
+   * Label at the top of the component (Average:)
+   * Value in the middle of the component (currentValue) with the unit (unit)
+   * Date at the bottom of the component (currentDate)
+   *
+   * These values are animated using the AnimatedText component
+   */
+  return (
+    <View style={styles.averageWeightContainer}>
+      <Text
+        style={[
+          styles.averageLabel,
+          {color: currentTheme.lightText, marginBottom: margins.xSmall},
+        ]}>
+        Average:
+      </Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          marginBottom: margins.small,
+        }}>
+        <AnimatedText
+          text={currentValue}
+          style={[
+            styles.averageValue,
+            {
+              color: currentTheme.text,
+            },
+          ]}
+        />
+        <Text
+          style={[
+            styles.averageWeightLabel,
+            {color: currentTheme.lightText, fontSize: fontSizes.large},
+          ]}>
+          {' '}
+          {unit}
+        </Text>
+      </View>
+
+      <AnimatedText
+        text={currentDate}
+        style={[
+          styles.averageWeightLabel,
+          {
+            color: currentTheme.lightText,
+            marginTop: -margins.small,
+          },
+        ]}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  chartView: {
+    //height: 200,
+    flex: 1,
+  },
+  averageLabel: {
+    fontSize: fontSizes.small,
+  },
+  valueContainer: {
+    height: 50,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  averageWeightContainer: {
+    width: '90%',
+    height: 100,
+    //backgroundColor: 'red',
+  },
+  averageWeightLabel: {
+    fontSize: fontSizes.medium,
+  },
+  averageValue: {
+    fontSize: fontSizes.title,
+    fontWeight: fontWeights.ultraBold,
+    color: 'black',
+  },
+});
---
+++ src/components/graphs/Line/types.ts
@@ -0,0 +1,92 @@
+// Type definitions for graph data
+
+// Utils
+import {format} from 'date-fns';
+
+export type graphDataPoint = {
+  /*
+   * graphDataPoint is a class that represents a single point of data for a graph.
+   * It contains a date and a value.
+   */
+  date: number;
+  value: number | null;
+};
+
+export class GraphPlotData {
+  /*
+   * GraphPlotData is a class that takes in an array of graphDataPoints and
+   * calculates the average value and period label for the data.
+   *
+   * @param data an array of graphDataPoints
+   * @param unit the unit of the data
+   * @param averageValue the average value of the data
+   * @param averagePeriodLabel the average period label of the data
+   */
+  data: graphDataPoint[];
+  unit: string;
+  averageValue: number;
+  averagePeriodLabel: string;
+
+  /**
+   * getAverageValue calculates the average value of the data
+   * @param data an array of graphDataPoints
+   * @returns the average value of the data
+   *
+   * @example
+   * // returns 1.5
+   * getAverageValue([
+   * { date: new Date(2020, 0, 1), value: 1 },
+   * { date: new Date(2020, 12, 31), value: 2 }
+   * ]);
+   */
+  getAverageValue() {
+    let sum = 0;
+    let count = 0;
+    for (let i = 0; i < this.data.length; i++) {
+      let current = this.data[i].value;
+      // If the value is null, do not include it in the average
+      if (current !== null) {
+        sum += current;
+      }
+      count++;
+    }
+    return sum / count;
+  }
+
+  /**
+   * getAveragePeriodLabel calculates the average period label of the data
+   * Assumes that the data is sorted by date
+   *
+   * @param data an array of graphDataPoints
+   * @returns the average period label of the data
+   *
+   * @example
+   * // returns '1 Jan - 31 Dec 2020'
+   * getAveragePeriodLabel([
+   *  { date: new Date(2020, 0, 1), value: 1 },
+   * { date: new Date(2020, 12, 31), value: 2 }
+   * ]);
+   *
+   * */
+  getAveragePeriodLabel() {
+    // If there is no data, return empty string
+    if (this.data.length === 0) {
+      return '';
+    }
+    // If there is only one data point, return the date
+    if (this.data.length === 1) {
+      return `${format(this.data[0].date, 'd MMM yyyy')}`;
+    }
+    // If there are multiple data points, return the first and last date
+    const firstDate = this.data[0].date;
+    const lastDate = this.data[this.data.length - 1].date;
+    return `${format(firstDate, 'd MMM')} - ${format(lastDate, 'd MMM yyyy')}`;
+  }
+
+  constructor(data: graphDataPoint[], unit: string) {
+    this.unit = unit;
+    this.data = data;
+    this.averageValue = this.getAverageValue();
+    this.averagePeriodLabel = this.getAveragePeriodLabel();
+  }
+}
