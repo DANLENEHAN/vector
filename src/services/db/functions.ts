6a0bc7d13cb45c67d936330c539d5026c0de72a5
Dan <daniel.lenehan.work@gmail.com>
2024-01-11 11:49:59 +0000

    Multiple rows work

--- src/services/db/functions.ts
+++ src/services/db/functions.ts
@@ -14,7 +14,16 @@ import {dbName, RowData} from './types';
 import {revisionObject} from './vectorRevisions';
 import {getAllTables} from './queries/other';
 
-const getDocumentDirectoryPath = () => {
+/**
+ * @description Retrieves the document directory path using React Native File System (RNFS).
+ *
+ * @returns {void} This function doesn't return a value; it logs the document directory path.
+ *
+ * @example
+ * // Example usage:
+ * getDocumentDirectoryPath();
+ */
+const getDocumentDirectoryPath = (): void => {
   try {
     const documentDirectoryPath = RNFS.DocumentDirectoryPath;
     console.log('Document Directory Path:', documentDirectoryPath);
@@ -23,6 +32,18 @@ const getDocumentDirectoryPath = () => {
   }
 };
 
+/**
+ * @description Represents the SQLite database instance.
+ *
+ * @type {SQLiteDatabase}
+ *
+ * @example
+ * // Example usage:
+ * // Access the database instance using the 'db' constant.
+ * db.transaction((tx: Transaction) => {
+ *   // Perform transactions or execute SQL commands here.
+ * });
+ */
 export const db: SQLiteDatabase = SQLite.openDatabase(
   {name: dbName},
   getDocumentDirectoryPath,
@@ -31,6 +52,17 @@ export const db: SQLiteDatabase = SQLite.openDatabase(
   },
 );
 
+/**
+ * @description Retrieves a subset of revisionObject starting from the specified key.
+ *
+ * @param startKey The key from which to start retrieving values. If null, retrieves all values.
+ * @returns {Object} An object representing key-value pairs from revisionObject starting from the specified key.
+ *
+ * @example
+ * // Example usage:
+ * const startingKey: string | null = '123';
+ * const subset = getKeyValuesStartingFrom(startingKey);
+ */
 export const getKeyValuesStartingFrom = (
   startKey: string | null,
 ): {[key: string]: string[]} => {
@@ -48,7 +80,19 @@ export const getKeyValuesStartingFrom = (
   }, {} as {[key: string]: string[]});
 };
 
-export const runMigrations = (revisionId: string | null) => {
+/**
+ * @description Runs database migrations starting from the specified revision ID.
+ * It processes revisions and executes corresponding SQL commands.
+ *
+ * @param revisionId The starting revision ID for migrations. If null, starts from the beginning.
+ * @returns {void} This function doesn't return a promise as it operates synchronously.
+ *
+ * @example
+ * // Example usage:
+ * const startingRevisionId: string | null = '123';
+ * runMigrations(startingRevisionId);
+ */
+export const runMigrations = (revisionId: string | null): void => {
   try {
     const revisionsToProcess = getKeyValuesStartingFrom(revisionId);
     if (Object.keys(revisionsToProcess).length === 0) {
@@ -75,7 +119,23 @@ export const runMigrations = (revisionId: string | null) => {
   }
 };
 
-export const getCurrentRevision = (callback: RevisionCallback) => {
+/**
+ * @description Retrieves the current revision ID from the alembic table.
+ *
+ * @param callback A callback function to handle the retrieved revision ID.
+ * @returns {void} This function doesn't return a promise as it operates synchronously.
+ *
+ * @example
+ * // Example usage:
+ * getCurrentRevision((revisionId) => {
+ *   if (revisionId) {
+ *     console.log('Current revision ID:', revisionId);
+ *   } else {
+ *     console.log('No revision ID found. Starting from scratch...');
+ *   }
+ * });
+ */
+export const getCurrentRevision = (callback: RevisionCallback): void => {
   db.transaction((tx: Transaction) => {
     tx.executeSql(
       `SELECT version_num FROM ${alembicTable};`,
@@ -97,7 +157,17 @@ export const getCurrentRevision = (callback: RevisionCallback) => {
   });
 };
 
-export const deleteDB = () => {
+/**
+ * @description Deletes all tables in the database.
+ * This function retrieves all table names, generates DROP TABLE queries, and executes them.
+ *
+ * @returns {void} This function doesn't return a promise as it operates synchronously.
+ *
+ * @example
+ * // Example usage:
+ * deleteDB();
+ */
+export const deleteDB = (): void => {
   console.log('Deleting DB tables. Hold on tight!');
   db.transaction((tx: Transaction) => {
     tx.executeSql(
@@ -124,7 +194,25 @@ export const deleteDB = () => {
   });
 };
 
-export const insertRows = async (tableName: string, data: RowData[]) => {
+/**
+ * @description Inserts rows into the specified table using the provided data.
+ *
+ * @param tableName The name of the table where the rows will be inserted.
+ * @param data An array of RowData representing the rows to be inserted.
+ * @returns {Promise<void>} A promise that resolves when the rows are successfully inserted.
+ * @throws {Error} Throws an error with a message if no data is provided for insertion.
+ * @throws {SQLError} Throws an error with details if the SQL execution encounters an issue.
+ *
+ * @example
+ * // Example usage:
+ * const tableName = 'my_table';
+ * const data: RowData[] = [...]; // An array of RowData representing rows
+ * await insertRows(tableName, data);
+ */
+export const insertRows = async (
+  tableName: string,
+  data: RowData[],
+): Promise<void> => {
   if (data.length === 0) {
     throw Error('No data to insert.');
   }
@@ -133,16 +221,23 @@ export const insertRows = async (tableName: string, data: RowData[]) => {
 
   db.transaction((tx: Transaction) => {
     const columns = schema.join(', ');
-    const placeholders = schema.map(() => '?').join(', ');
-    const insertValues = data.map(item => schema.map(col => item[col])).flat();
+    const placeholders = data
+      .map(
+        () =>
+          `(${Object.keys(schema)
+            .map(() => '?')
+            .join(', ')})`,
+      )
+      .join(', ');
+    const insertValues = data.flatMap(value => Object.values(value));
 
     tx.executeSql(
-      `INSERT INTO ${tableName} (${columns}) VALUES (${placeholders})`,
+      `INSERT INTO ${tableName} (${columns}) VALUES ${placeholders}`,
       insertValues,
       () => {
         console.log(
-          `Successfulling inserted ${insertValues.length} row${
-            's' ? insertValues.length > 1 : ''
+          `Successfulling inserted ${data.length} row${
+            data.length > 1 ? 's' : ''
           } in '${tableName}'.`,
         );
       },
