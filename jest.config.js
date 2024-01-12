814235c83d0463460d097b0415c05b8c6e1d5548
Daniel Maguire <dmaguire1996@gmail.com>
2024-01-12 12:12:24 +0000

    Updated jest configs

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,15 @@ module.exports = {
     'node_modules/(?!(@react-native|react-native|react-native-vector-icons)/)',
   ],
   moduleNameMapper: {
-    '^@components/(.*)$': '<rootDir>/src/components/$1',
-    '^@context/(.*)$': '<rootDir>/src/context/$1',
-    '^@navigation/(.*)$': '<rootDir>/src/navigation/$1',
-    '^@screens/(.*)$': '<rootDir>/src/screens/$1',
-    '^@services/(.*)$': '<rootDir>/src/services/$1',
-    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
-    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
-    '^@assets/(.*)$': '<rootDir>/assets/$1',
+    '@components/(.*)$': '<rootDir>/src/components/$1',
+    '@context/(.*)$': '<rootDir>/src/context/$1',
+    '@navigation/(.*)$': '<rootDir>/src/navigation/$1',
+    '@screens/(.*)$': '<rootDir>/src/screens/$1',
+    '@services/(.*)$': '<rootDir>/src/services/$1',
+    '@styles/(.*)$': '<rootDir>/src/styles/$1',
+    '@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '@assets/(.*)$': '<rootDir>/assets/$1',
   },
 };
+
+console.log('jest.config.js');
