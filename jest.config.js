35bdc8028e79287d7b1ad3a6895324b5e677356a
Daniel Maguire <dmaguire1996@gmail.com>
2024-01-12 11:55:38 +0000

    Updatedjest config

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,14 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(@react-native|react-native|react-native-vector-icons)/)',
   ],
+  moduleNameMapper: {
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@context/(.*)$': '<rootDir>/src/context/$1',
+    '^@navigation/(.*)$': '<rootDir>/src/navigation/$1',
+    '^@screens/(.*)$': '<rootDir>/src/screens/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@assets/(.*)$': '<rootDir>/assets/$1',
+  },
 };
