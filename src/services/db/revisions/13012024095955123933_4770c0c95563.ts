ec727911decd60ebca8dcf2cc8dea74ba2e426f3
Dan <daniel.lenehan.work@gmail.com>
2024-01-13 11:08:47 +0000

    removed superuser

--- src/services/db/revisions/13012024095955123933_4770c0c95563.ts
+++ src/services/db/revisions/13012024095955123933_4770c0c95563.ts
@@ -1,6 +1,6 @@
 export const revisionID = '4770c0c95563';
 
 export const sqlCommands_13012024095955123933_4770c0c95563: string[] = [
-  "INSERT INTO user_account (username, email, password, status, premium, age, birthday, first_name, last_name, gender, phone_number, height_unit_pref, weight_unit_pref, date_format_pref, goal, superuser, created_at, updated_at) VALUES ('ceo', 'dan_ceo@gmail.com', 'gAAAAABlol8buiAxlt98ErhRyymV1QlWrcpiUUPZ5Z7KQVm_obefCf86bQZzNGb0LUK6u7c3KE8v0ZBNxvsm6xxgUGxVI0_aeYDxz-I2an7czx4joiWQu8k=', 'active', true, 33, '1997-05-18', 'Daniel', 'Lenehan', 'male', '1234567890', 'cm', 'kg', '%d-%m-%Y', 'build_muscle', true, '2024-01-13 09:59:55.395892', '2024-01-13 09:59:55.395895');",
+  // Removed superuser insert here
   "UPDATE alembic_version SET version_num='4770c0c95563' WHERE alembic_version.version_num = '8f5c9144791d';",
 ];
