63beafb809656c85086f0f353c81bb00831fffa7
Dan <daniel.lenehan.work@gmail.com>
2024-01-23 15:05:04 +0000

    Final Test

---
+++ test/db/sync/SyncProcess.test.ts
@@ -0,0 +1,64 @@
+// Constants
+import {apiFunctions} from '@services/db/sync/Constants';
+import {dbTables} from '@shared/Constants';
+
+// Types
+import {SyncOperation} from '@shared/enums';
+
+// Functions
+import {runSyncProcess} from '@services/db/sync/SyncProcess';
+import {
+  processSyncTypePull,
+  processSyncTypePush,
+} from '@services/db/sync/SyncTypes';
+
+/* Mocking external dependencies */
+jest.mock('react-native-fs', () => ({
+  DocumentDirectoryPath: '/mocked/document/directory/path',
+}));
+
+jest.mock('uuid', () => ({
+  uuidv4: '67f6127d-13cc-4c27-b91f-2b1f83c48eeb',
+}));
+
+jest.mock('react-native-sqlite-storage', () => ({
+  openDatabase: jest.fn(() => ({
+    transaction: jest.fn(),
+  })),
+}));
+/**/
+
+jest.mock('@services/db/sync/SyncTypes', () => ({
+  processSyncTypePush: jest.fn(),
+  processSyncTypePull: jest.fn(),
+}));
+
+describe('Sync Process Tests', () => {
+  test('runSyncProcess', async () => {
+    await runSyncProcess();
+
+    expect(processSyncTypePull).toHaveBeenCalledTimes(2);
+    expect(processSyncTypePull).toHaveBeenCalledWith(
+      dbTables.statTable,
+      apiFunctions[dbTables.statTable],
+      SyncOperation.Creates,
+    );
+    expect(processSyncTypePull).toHaveBeenCalledWith(
+      dbTables.statTable,
+      apiFunctions[dbTables.statTable],
+      SyncOperation.Updates,
+    );
+
+    expect(processSyncTypePush).toHaveBeenCalledTimes(2);
+    expect(processSyncTypePush).toHaveBeenCalledWith(
+      dbTables.statTable,
+      apiFunctions[dbTables.statTable],
+      SyncOperation.Creates,
+    );
+    expect(processSyncTypePush).toHaveBeenCalledWith(
+      dbTables.statTable,
+      apiFunctions[dbTables.statTable],
+      SyncOperation.Updates,
+    );
+  });
+});
