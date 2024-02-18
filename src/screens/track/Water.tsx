4be46e811ed2a449a3ea1288d891f0b8c527edb9
Dan <daniel.lenehan.work@gmail.com>
2024-02-18 06:33:57 +0000

    MAde Water and Weight Generic

--- src/screens/track/Water.tsx
+++ src/screens/track/Water.tsx
@@ -1,30 +1,17 @@
 // React imports
-import React, {useState} from 'react';
-import {
-  View,
-  StyleSheet,
-  Text,
-  TouchableWithoutFeedback,
-  Keyboard,
-} from 'react-native';
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
 // Components
-import Icon from 'react-native-vector-icons/FontAwesome6';
-import ButtonComponent from '@components/buttons/ButtonComponent';
-import UnitSelector from '@components/buttons/UnitSelector';
-import NumberInput from '@components/inputs/NumberInput';
 import Header from '@components/navbar/Header';
+import GenericMeasurementTracking from '@screens/track/bodyMeasurement/GenericMeasurementTracking';
+
 // Types
 import {ScreenProps} from '@screens/Types';
-import {NutritionType, WaterUnit} from '@services/api/swagger/data-contracts';
+import {NutritionType} from '@services/api/swagger/data-contracts';
 // Styling
-import {useSystem} from '@context/SystemContext';
-import {lightThemeColors, darkThemeColors} from '@styles/Main';
-import {layoutStyles, headingTextStyles} from '@styles/Main';
+import {layoutStyles} from '@styles/Main';
 // Services
-import {createNewNutrition} from '@services/api/blueprints/nutrition/Functions';
 import ScreenWrapper from '@components/layout/ScreenWrapper';
-// Logger
-import logger from '@utils/Logger';
 
 /**
  *  Water tracking screen
@@ -37,67 +24,18 @@ import logger from '@utils/Logger';
 const WaterScreen: React.FC<ScreenProps> = ({
   navigation,
 }: ScreenProps): React.ReactElement<ScreenProps> => {
-  const {theme} = useSystem();
-  const currentTheme = theme === 'dark' ? darkThemeColors : lightThemeColors;
-
-  // Variables to track the unit selection
-  const [activeUnit, setActiveUnit] = useState<string>(WaterUnit.Ml);
-  const [waterValue, setWaterValue] = useState<string>('0');
-
-  const handleSavedWater = async () => {
-    const parsedWater = parseFloat(waterValue);
-    // Validation: Check if the weight is 0 or the string is empty
-    if (isNaN(parsedWater) || parsedWater <= 0) {
-      logger.error('Invalid water value. Please enter a valid water value.');
-      return; // Stop the function if the weight is invalid
-    }
-    createNewNutrition({
-      value: parsedWater,
-      unitValue: activeUnit.toLowerCase() as WaterUnit,
-      onSuccessfulCreate: navigation.goBack,
-      statType: NutritionType.Water,
-    });
-  };
-
   return (
     <ScreenWrapper>
       <View style={styles.headerSection}>
         <Header onClick={navigation.goBack} includeBackArrow={true} />
       </View>
-      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-        <View style={styles.content}>
-          <Text
-            style={[
-              styles.title,
-              {
-                color: currentTheme.text,
-              },
-            ]}>
-            Hydration is key!
-          </Text>
-          <Icon
-            name={'glass-water'}
-            solid
-            size={250}
-            color={currentTheme.primary}
-          />
-          <NumberInput
-            allowFloat={false}
-            inputValue={waterValue}
-            setInputValue={setWaterValue}
-          />
-          <UnitSelector
-            units={Object.values(WaterUnit)}
-            activeUnit={activeUnit}
-            setActiveUnit={setActiveUnit}
-          />
-          <ButtonComponent
-            text="Track"
-            disabled={waterValue === '0' || waterValue === ''}
-            onPress={handleSavedWater}
-          />
-        </View>
-      </TouchableWithoutFeedback>
+      <View style={styles.componentContainer}>
+        <GenericMeasurementTracking
+          statType={NutritionType.Water}
+          onSuccessfulCreate={navigation.goBack}
+          statName={'Water'}
+        />
+      </View>
     </ScreenWrapper>
   );
 };
@@ -106,12 +44,9 @@ const styles = StyleSheet.create({
   headerSection: {
     flex: 1,
   },
-  content: {
-    flex: 9,
-    ...layoutStyles.spaceAroundVertical,
-  },
-  title: {
-    ...headingTextStyles.large,
+  componentContainer: {
+    flex: 15,
+    ...layoutStyles.centerVertically,
   },
 });
 
