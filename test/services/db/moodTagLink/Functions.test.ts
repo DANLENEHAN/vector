554b96c2ce75bb871049e36ed2f5e857b0bbed90
Daniel Maguire <dmaguire1996@gmail.com>
2024-02-26 11:21:35 +0000

    Merged Main + Ran alembic reset

--- test/services/db/moodTagLink/Functions.test.ts
+++ test/services/db/moodTagLink/Functions.test.ts
@@ -1,11 +1,38 @@
-import { insertMoodTagLinks } from "@services/db/moodTagLink/Functions";
+import {insertMoodTagLinks} from '@services/db/moodTagLink/Functions';
+import * as dbFunctions from '@services/db/Functions';
 
 describe('insertMoodTagLinks', () => {
-    beforeEach(() => {
-        // Clears 'toHaveBeenCalledTimes' cache
-        jest.clearAllMocks();
-    });
-    
-    it('insertMoodTagLinks works', async () => {
-    });
+  beforeEach(() => {
+    // Clears 'toHaveBeenCalledTimes' cache
+    jest.clearAllMocks();
+  });
+
+  it('insertMoodTagLinks works', async () => {
+    // Arrange
+    const moodTagLinks = [
+      {
+        mood_tag_link_id: '1',
+        mood_id: '1',
+        mood_tag_id: '1',
+        created_at: '2021-01-01T00:00:00.000Z',
+        timezone: 'UTC',
+      },
+      {
+        mood_tag_link_id: '2',
+        mood_id: '1',
+        mood_tag_id: '2',
+        created_at: '2021-01-01T00:00:00.000Z',
+        timezone: 'UTC',
+      },
+    ];
+    const insertRowsSpy = jest
+      .spyOn(dbFunctions, 'insertRows')
+      .mockResolvedValueOnce();
+
+    // Act
+    await insertMoodTagLinks(moodTagLinks);
+
+    // Assert
+    expect(insertRowsSpy).toHaveBeenCalledTimes(1);
+  });
 });
