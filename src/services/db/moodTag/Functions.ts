6c98164ceb94f0a48c6f31a54c409b18d845c00d
Daniel Maguire <dmaguire1996@gmail.com>
2024-02-26 11:49:47 +0000

    Added missing docstring

--- src/services/db/moodTag/Functions.ts
+++ src/services/db/moodTag/Functions.ts
@@ -3,6 +3,13 @@ import {MoodTagCreateSchema} from '@services/api/swagger/data-contracts';
 // Functions
 import {executeSqlBatch} from '@services/db/SqlClient';
 
+/**
+ * Function to get all mood tags from the database.
+ *
+ * @returns {Promise<MoodTagCreateSchema[]>} A promise that resolves to an array of mood tags.
+ * @throws {Error} Throws an error if there's a problem executing the SQL queries.
+ * @throws {Error} Throws an error if no mood tags are found.
+ */
 export const getMoodTags = async (): Promise<MoodTagCreateSchema[]> => {
   const query = {
     sqlStatement: 'SELECT * FROM mood_tag;',
