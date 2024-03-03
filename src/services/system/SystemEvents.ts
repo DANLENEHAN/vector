505f71714c9265265bc0f148e98a3bdf09cf1684
Dan <daniel.lenehan.work@gmail.com>
2024-03-03 12:58:29 +0000

    Code Move

--- src/services/system/SystemEvents.ts
+++ src/services/system/SystemEvents.ts
@@ -3,21 +3,15 @@ import {handleClientSessionEvent} from '@services/api/blueprints/clientSessionEv
 import {runSyncProcess} from '@services/db/sync/SyncProcess';
 import {retrieveOrRegisterDeviceId} from '@services/api/blueprints/device/Functions';
 import {getUser} from '@services/db/user/Functions';
+import {isFirstAppEntryToday} from './Functions';
 // Types
 import {AppEntryType} from '@services/system/Types';
 import {
-  BaseOperators,
-  ClientSessionEventCreateSchema,
   ClientSessionEventType,
-  NumericOperators,
   UserCreateSchema,
 } from '@services/api/swagger/data-contracts';
 // Logger
 import logger from '@utils/Logger';
-import {getRows} from '@services/db/Operations';
-import {syncDbTables, timestampFields} from '@shared/Constants';
-import {deviceTimestampNow, getDayBoundsOfDate} from '@services/date/Functions';
-import {DayBounds} from '@services/date/Type';
 import {
   checkStreakBreak,
   registerStreakNotifcation,
@@ -86,44 +80,4 @@ export const appEntryCallback = async (
   if (isFirstAppEntry) {
     await registerStreakNotifcation();
   }
-};
-
-/**
- * Determines if the current app entry is the first for the user today. This function calculates
- * the bounds of the current day based on the device's local time and queries the database for any
- * app open events within these bounds. It considers the app entry to be the first of the day if
- * exactly one app open event is found within the current day's boundaries, indicating no prior
- * app entries have been recorded for the day.
- *
- * The function relies on a generic `getRows` function for querying the database, which is configured
- * to retrieve rows from a specific table based on the provided conditions. These conditions are
- * tailored to identify app open events that occurred within the start and end of the current day,
- * using the device's timestamp to define the day's boundaries.
- *
- * @async
- * @returns {Promise<boolean>} A promise that resolves to `true` if this is the first app entry of
- *                             the day, or `false` otherwise. The determination is based on the
- *                             presence of exactly one app open event within the day's boundaries.
- *
- * @throws {Error} Implicitly handles errors through the return value. It relies on the underlying
- *                 database query mechanism to manage errors, such as network issues or data access
- *                 problems, which could affect the accuracy of the result. The function ensures that
- *                 any failure in querying is caught and does not disrupt the application's flow.
- */
-export const isFirstAppEntryToday = async (): Promise<boolean> => {
-  const dayBounds: DayBounds = getDayBoundsOfDate(deviceTimestampNow());
-  const appOpenEvent = await getRows<ClientSessionEventCreateSchema>({
-    tableName: syncDbTables.clientSessionEventTable,
-    selectColumns: [`${syncDbTables.clientSessionEventTable}_id`],
-    whereConditions: {
-      [timestampFields.createdAt]: {
-        [NumericOperators.Ge]: dayBounds.startOfDay,
-        [NumericOperators.Le]: dayBounds.endOfDay,
-      },
-      event_type: {
-        [BaseOperators.Eq]: ClientSessionEventType.AppOpen,
-      },
-    },
-  });
-  return appOpenEvent !== null && appOpenEvent.length === 1;
 };
--- test/services/system/Functions.test.ts
+++ test/services/system/Functions.test.ts
@@ -1,9 +1,17 @@
 // Functions
-import {getDeviceInfo} from '@services/system/Functions';
+import {isFirstAppEntryToday, getDeviceInfo} from '@services/system/Functions';
 import logger from '@utils/Logger';
+import * as DbOperationsFunctions from '@services/db/Operations';
+import * as DateFunctions from '@services/date/Functions';
 
 // Types
 import {SessionEventDeviceInfo} from '@services/system/Types';
+import {syncDbTables, timestampFields} from '@shared/Constants';
+import {
+  BaseOperators,
+  ClientSessionEventType,
+  NumericOperators,
+} from '@services/api/swagger/data-contracts';
 
 jest.mock('react-native-device-info', () => ({
   getUniqueId: jest.fn().mockResolvedValue('mockedDeviceId'),
@@ -18,7 +26,24 @@ jest.mock('react-native-device-info', () => ({
   getVersion: jest.fn().mockReturnValue('mockedVersion'),
 }));
 
+jest.mock('@services/date/Functions', () => ({
+  ...jest.requireActual('@services/date/Functions'),
+  getDayBoundsOfDate: jest
+    .fn()
+    .mockReturnValue({startOfDay: 'startOfDay', endOfDay: 'endOfDay'}),
+  deviceTimestampNow: jest.fn().mockReturnValue('fakeTimeNow'),
+}));
+
+jest.mock('@services/db/Operations.ts', () => ({
+  ...jest.requireActual('@services/db/Operations.ts'),
+  getRows: jest.fn(),
+}));
+
 describe('System Functions Tests', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   const deviceInfo: SessionEventDeviceInfo = {
     brand: 'mockedBrand',
     deviceId: 'mockedDeviceId',
@@ -26,6 +51,85 @@ describe('System Functions Tests', () => {
     userAgent: 'mockedUserAgent',
     version: 'mockedVersion',
   };
+
+  const getRowParams = {
+    tableName: syncDbTables.clientSessionEventTable,
+    selectColumns: [`${syncDbTables.clientSessionEventTable}_id`],
+    whereConditions: {
+      [timestampFields.createdAt]: {
+        [NumericOperators.Ge]: 'startOfDay',
+        [NumericOperators.Le]: 'endOfDay',
+      },
+      event_type: {
+        [BaseOperators.Eq]: ClientSessionEventType.AppOpen,
+      },
+    },
+  };
+
+  test('isFirstAppEntryToday - is first entry', async () => {
+    // Arrange
+    jest.spyOn(DbOperationsFunctions, 'getRows').mockResolvedValueOnce([
+      {
+        client_session_event: 'fakeId',
+      },
+    ]);
+
+    // Act
+    const response = await isFirstAppEntryToday();
+
+    // Assert
+    expect(DateFunctions.getDayBoundsOfDate).toHaveBeenCalledTimes(1);
+    expect(DateFunctions.getDayBoundsOfDate).toHaveBeenCalledWith(
+      'fakeTimeNow',
+    );
+    expect(DbOperationsFunctions.getRows).toHaveBeenCalledTimes(1);
+    expect(DbOperationsFunctions.getRows).toHaveBeenCalledWith(getRowParams);
+    expect(response).toEqual(true);
+  });
+
+  test("isFirstAppEntryToday - isn't first entry", async () => {
+    // Arrange
+    jest
+      .spyOn(DbOperationsFunctions, 'getRows')
+      .mockResolvedValueOnce([
+        {client_session_event: 'fakeId'},
+        {client_session_event: 'fakeIdTwo'},
+      ]);
+
+    // Act
+    const response = await isFirstAppEntryToday();
+
+    // Assert
+    expect(DateFunctions.getDayBoundsOfDate).toHaveBeenCalledTimes(1);
+    expect(DateFunctions.getDayBoundsOfDate).toHaveBeenCalledWith(
+      'fakeTimeNow',
+    );
+    expect(DbOperationsFunctions.getRows).toHaveBeenCalledTimes(1);
+    expect(DbOperationsFunctions.getRows).toHaveBeenCalledWith(getRowParams);
+    expect(response).toEqual(false);
+  });
+
+  test("isFirstAppEntryToday - isn't first entry", async () => {
+    // Arrange
+    jest
+      .spyOn(DbOperationsFunctions, 'getRows')
+      .mockResolvedValueOnce([
+        {client_session_event: 'fakeId'},
+        {client_session_event: 'fakeIdTwo'},
+      ]);
+
+    // Act
+    const response = await isFirstAppEntryToday();
+
+    // Assert
+    expect(DateFunctions.getDayBoundsOfDate).toHaveBeenCalledTimes(1);
+    expect(DateFunctions.getDayBoundsOfDate).toHaveBeenCalledWith(
+      'fakeTimeNow',
+    );
+    expect(DbOperationsFunctions.getRows).toHaveBeenCalledTimes(1);
+    expect(DbOperationsFunctions.getRows).toHaveBeenCalledWith(getRowParams);
+    expect(response).toEqual(false);
+  });
 
   test('getDeviceInfo apis return no errors', async () => {
     // Arrange
