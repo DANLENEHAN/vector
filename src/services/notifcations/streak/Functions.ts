cbb0a6ebbc37f563634ee82bd9d7ee8e679c3b4d
Dan <daniel.lenehan.work@gmail.com>
2024-03-03 11:09:44 +0000

    Docstrings

--- src/services/notifcations/streak/Functions.ts
+++ src/services/notifcations/streak/Functions.ts
@@ -20,14 +20,29 @@ import {deviceTimestampNow, getDayBoundsOfDate} from '@services/date/Functions';
 import moment from 'moment-timezone';
 import {StreakNotifcationId} from './Constants';
 
-export const registerStreakNotifcation = async () => {
-  // Get Streak
-  // call getStreak()
-  // Build Notifcation Object
-  // Scheduled Tomorrow at 9:00am local time
-  // Message Extended Your Streak to Streak + 1
-  // Send Streak
-
+/**
+ * Schedules a local notification to encourage the user to maintain their login streak by logging in
+ * before midnight. It calculates the notification time as 9:00 AM the following day from the current
+ * device timestamp. The function retrieves the current streak using `getStreak`, and, if successful,
+ * schedules a notification using `PushNotification.localNotificationSchedule`. The notification
+ * message dynamically reflects the user's current streak and prompts the user to log in again to
+ * extend their streak. This function is part of a larger effort to enhance user engagement through
+ * daily reminders.
+ *
+ * Utilizes the `moment.js` library for date manipulation and relies on the `PushNotification` module
+ * for scheduling the notification. It logs warnings and information regarding the operation's
+ * success or failure using a `logger`.
+ *
+ * @async
+ * @returns {Promise<void>} - A promise that resolves when the notification has been scheduled or
+ *                            when it cannot be scheduled due to a missing streak. It does not return
+ *                            any value upon resolution.
+ *
+ * @throws {Error} - If there are issues retrieving the streak or scheduling the notification, it
+ *                   logs the error using `logger.warn` instead of throwing it to avoid breaking
+ *                   the application flow.
+ */
+export const registerStreakNotifcation = async (): Promise<void> => {
   const tomorrow = deviceTimestampNow().add(1, 'days');
   const scheduleDate = new Date(
     tomorrow.year(),
@@ -53,7 +68,27 @@ export const registerStreakNotifcation = async () => {
   }
 };
 
-export const getStreak = async () => {
+/**
+ * Retrieves and calculates the current login streak of the user based on app open events recorded
+ * in the database. It first fetches the most recent app open event to establish the end time of the
+ * streak calculation. Then, it attempts to find the latest streak break event or, failing that, the
+ * very first app open event to establish the start time of the streak. The streak is calculated as
+ * the number of full days between these two events.
+ *
+ * This function employs `moment.js` for date calculations and leverages a generic `getRows` function
+ * tailored for database queries within the application's infrastructure, which requires specifying
+ * table names, columns to select, where conditions, order conditions, and limit for the query.
+ *
+ * @async
+ * @returns {Promise<number | null>} A promise that resolves to the number of full days representing
+ *                                    the user's current streak, or null if it cannot be calculated due
+ *                                    to the absence of necessary app open events.
+ *
+ * @throws {Error} Implicitly handles errors by logging warning messages and returning null instead of
+ *                 throwing errors directly. This is to avoid disrupting the application flow and to
+ *                 handle missing data gracefully.
+ */
+export const getStreak = async (): Promise<number | null> => {
   // Get the Latest App Open
   const latestAppOpen = await getRows<ClientSessionEventCreateSchema>({
     tableName: syncDbTables.clientSessionEventTable,
@@ -117,17 +152,29 @@ export const getStreak = async () => {
   return moment(endTime).diff(moment(startTime), 'days');
 };
 
+/**
+ * Checks whether the user failed to log in the previous day, potentially indicating a streak break.
+ * This function computes the start and end boundaries of the previous day and queries the database
+ * for any app open events within this time frame. If no app open events are found, it concludes that
+ * the user did not log in the previous day, triggering a streak break event by calling
+ * `handleClientSessionEvent` with the `ClientSessionEventType.StreakBreak` event type.
+ *
+ * This process utilizes `moment.js` for date manipulation to determine the day boundaries and a
+ * generic `getRows` function for database queries, which is designed to be flexible across different
+ * types of data retrieval scenarios in the application's infrastructure.
+ *
+ * @async
+ * @returns {Promise<void>} A promise that resolves when the check is complete and the streak break
+ *                          has been handled if necessary. There is no value returned upon resolution,
+ *                          but a streak break event may be recorded, or an informational log may be
+ *                          generated indicating the user's login activity.
+ *
+ * @throws {Error} Implicitly handles errors through the logging of informational messages or by the
+ *                 graceful handling of no results found in database queries, rather than throwing
+ *                 errors directly. This approach is chosen to avoid disrupting the application flow
+ *                 and to manage the potential absence of login data in a user-friendly manner.
+ */
 export const checkStreakBreak = async () => {
-  // Get Latest AppOpen (UTC)
-  // Get Local Time of Latest AppOpen
-  // Get Local Time Bounds of Yesterday
-  // Get UTC time of Bounds
-  // Check if there was an App Open within the Bounds
-  // If there was one do nothing
-  // If there was none. Insert a Streak Break
-  // Streak break must be EOD local time
-  // Streak break of local must be converted to UTC before inserting
-
   const timestampNow = deviceTimestampNow();
   const yesterday = timestampNow.clone().subtract(1, 'days');
   const dayBounds: DayBounds = getDayBoundsOfDate(yesterday);
--- src/services/system/SystemEvents.ts
+++ src/services/system/SystemEvents.ts
@@ -23,6 +23,35 @@ import {
   registerStreakNotifcation,
 } from '@services/notifcations/streak/Functions';
 
+/**
+ * Handles various app entry events, logging the entry type, checking and updating user session
+ * events, and managing streak notifications based on the type of app entry. This function serves
+ * multiple purposes based on the app entry type parameter it receives, which includes logging
+ * the app entry event, ensuring that a user session event for app opening is recorded, checking
+ * for streak breaks if it's the first app entry of the day, handling specific events related to
+ * account creation and login, running a synchronization process unless the entry type is an
+ * offline token login, and finally, registering a notification for the streak if it's the first
+ * entry of the day.
+ *
+ * The function employs several asynchronous operations to interact with the user's session data,
+ * check for streak continuity, and update the device registration as necessary. It is designed to
+ * work seamlessly with other components of the app's session management and user engagement
+ * strategies.
+ *
+ * @param {AppEntryType} appEntryType - The type of app entry event, which dictates the specific
+ *                                      actions to be taken by the function.
+ *
+ * @async
+ * @returns {Promise<void>} A promise that resolves once all the operations triggered by the app
+ *                          entry type have been executed. It does not return any value upon
+ *                          resolution, but it performs several updates and checks related to the
+ *                          user's session and engagement with the app.
+ *
+ * @throws {Error} While the function itself does not explicitly throw errors, it relies on
+ *                 asynchronous operations that may throw errors due to database access issues,
+ *                 network problems, or other unforeseen circumstances. These are handled
+ *                 internally within the called functions, ensuring the app's flow is not disrupted.
+ */
 export const appEntryCallback = async (appEntryType: AppEntryType) => {
   logger.info(`App Entry Event. Type: '${appEntryType}'`);
 
@@ -57,6 +86,28 @@ export const appEntryCallback = async (appEntryType: AppEntryType) => {
   }
 };
 
+/**
+ * Determines if the current app entry is the first for the user today. This function calculates
+ * the bounds of the current day based on the device's local time and queries the database for any
+ * app open events within these bounds. It considers the app entry to be the first of the day if
+ * exactly one app open event is found within the current day's boundaries, indicating no prior
+ * app entries have been recorded for the day.
+ *
+ * The function relies on a generic `getRows` function for querying the database, which is configured
+ * to retrieve rows from a specific table based on the provided conditions. These conditions are
+ * tailored to identify app open events that occurred within the start and end of the current day,
+ * using the device's timestamp to define the day's boundaries.
+ *
+ * @async
+ * @returns {Promise<boolean>} A promise that resolves to `true` if this is the first app entry of
+ *                             the day, or `false` otherwise. The determination is based on the
+ *                             presence of exactly one app open event within the day's boundaries.
+ *
+ * @throws {Error} Implicitly handles errors through the return value. It relies on the underlying
+ *                 database query mechanism to manage errors, such as network issues or data access
+ *                 problems, which could affect the accuracy of the result. The function ensures that
+ *                 any failure in querying is caught and does not disrupt the application's flow.
+ */
 export const isFirstAppEntryToday = async (): Promise<boolean> => {
   const dayBounds: DayBounds = getDayBoundsOfDate(deviceTimestampNow());
   const appOpenEvent = await getRows<ClientSessionEventCreateSchema>({
