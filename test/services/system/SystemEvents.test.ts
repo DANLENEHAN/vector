d5d738248ddd5b2ff5c29a43357f648a5eda2577
Dan <daniel.lenehan.work@gmail.com>
2024-03-06 13:56:44 +0000

    Fixing Tests

--- test/services/system/SystemEvents.test.ts
+++ test/services/system/SystemEvents.test.ts
@@ -1,24 +1,16 @@
 // Functions
 import * as SystemEventFunctions from '@services/system/SystemEvents';
 import * as SystemFunctions from '@services/system/Functions';
-import * as UserFunctions from '@services/db/user/Functions';
 import * as ClientSessionEventFunctions from '@services/api/blueprints/clientSessionEvent/Functions';
 import * as StreakFunctions from '@services/notifcations/streak/Functions';
 import * as SyncProcessFunctions from '@services/db/sync/SyncProcess';
-import * as DeviceFunctions from '@services/api/blueprints/device/Functions';
 import * as StreakEvents from '@services/notifcations/streak/Events';
 
 // Types
 import {ClientSessionEventType} from '@services/api/swagger/data-contracts';
 
 // Test Objects
-import {sampleUser} from '../../Objects';
 import {AppEntryType} from '@services/system/Types';
-
-jest.mock('@services/db/user/Functions', () => ({
-  ...jest.requireActual('@services/db/user/Functions'),
-  getUser: jest.fn(),
-}));
 
 jest.mock('@services/api/blueprints/clientSessionEvent/Functions', () => ({
   ...jest.requireActual(
@@ -42,11 +34,6 @@ jest.mock('@services/db/sync/SyncProcess', () => ({
   runSyncProcess: jest.fn().mockResolvedValue(null),
 }));
 
-jest.mock('@services/api/blueprints/device/Functions', () => ({
-  ...jest.requireActual('@services/api/blueprints/device/Functions'),
-  retrieveOrRegisterDeviceId: jest.fn().mockResolvedValue(null),
-}));
-
 describe('SystemEvents Functions Tests', () => {
   beforeEach(() => {
     jest.clearAllMocks();
@@ -54,7 +41,6 @@ describe('SystemEvents Functions Tests', () => {
 
   test('appEntryCallback - Type LoginTokenOnline', async () => {
     // Arrange
-    jest.spyOn(UserFunctions, 'getUser').mockResolvedValueOnce(sampleUser);
     jest
       .spyOn(SystemFunctions, 'isFirstAppEntryToday')
       .mockResolvedValueOnce(true);
@@ -65,7 +51,6 @@ describe('SystemEvents Functions Tests', () => {
     );
 
     // Assert
-    expect(UserFunctions.getUser).toHaveBeenCalledTimes(1);
     expect(
       ClientSessionEventFunctions.handleClientSessionEvent,
     ).toHaveBeenCalledTimes(1);
@@ -75,14 +60,12 @@ describe('SystemEvents Functions Tests', () => {
     expect(SystemFunctions.isFirstAppEntryToday).toHaveBeenCalledTimes(1);
     expect(StreakFunctions.checkStreakBreak).toHaveBeenCalledTimes(1);
     expect(SyncProcessFunctions.runSyncProcess).toHaveBeenCalledTimes(1);
-    expect(DeviceFunctions.retrieveOrRegisterDeviceId).toHaveBeenCalledTimes(1);
     expect(StreakEvents.registerStreakNotifcation).toHaveBeenCalledTimes(1);
     expect(response).toEqual(undefined);
   });
 
   test('appEntryCallback - Type LoginTokenOnline, is not first app entry', async () => {
     // Arrange
-    jest.spyOn(UserFunctions, 'getUser').mockResolvedValueOnce(sampleUser);
     jest
       .spyOn(SystemFunctions, 'isFirstAppEntryToday')
       .mockResolvedValueOnce(false);
@@ -93,7 +76,6 @@ describe('SystemEvents Functions Tests', () => {
     );
 
     // Assert
-    expect(UserFunctions.getUser).toHaveBeenCalledTimes(1);
     expect(
       ClientSessionEventFunctions.handleClientSessionEvent,
     ).toHaveBeenCalledTimes(1);
@@ -103,42 +85,12 @@ describe('SystemEvents Functions Tests', () => {
     expect(SystemFunctions.isFirstAppEntryToday).toHaveBeenCalledTimes(1);
     expect(StreakFunctions.checkStreakBreak).toHaveBeenCalledTimes(0);
     expect(SyncProcessFunctions.runSyncProcess).toHaveBeenCalledTimes(1);
-    expect(DeviceFunctions.retrieveOrRegisterDeviceId).toHaveBeenCalledTimes(1);
     expect(StreakEvents.registerStreakNotifcation).toHaveBeenCalledTimes(0);
-    expect(response).toEqual(undefined);
-  });
-
-  test('appEntryCallback - Type LoginTokenOnline, getUser returns null', async () => {
-    // Arrange
-    jest.spyOn(UserFunctions, 'getUser').mockResolvedValueOnce(null);
-    jest
-      .spyOn(SystemFunctions, 'isFirstAppEntryToday')
-      .mockResolvedValueOnce(true);
-
-    // Act
-    const response = await SystemEventFunctions.appEntryCallback(
-      AppEntryType.LoginTokenOnline,
-    );
-
-    // Assert
-    expect(UserFunctions.getUser).toHaveBeenCalledTimes(1);
-    expect(
-      ClientSessionEventFunctions.handleClientSessionEvent,
-    ).toHaveBeenCalledTimes(1);
-    expect(
-      ClientSessionEventFunctions.handleClientSessionEvent,
-    ).toHaveBeenCalledWith(ClientSessionEventType.AppOpen);
-    expect(SystemFunctions.isFirstAppEntryToday).toHaveBeenCalledTimes(1);
-    expect(StreakFunctions.checkStreakBreak).toHaveBeenCalledTimes(1);
-    expect(SyncProcessFunctions.runSyncProcess).toHaveBeenCalledTimes(1);
-    expect(DeviceFunctions.retrieveOrRegisterDeviceId).toHaveBeenCalledTimes(0);
-    expect(StreakEvents.registerStreakNotifcation).toHaveBeenCalledTimes(1);
     expect(response).toEqual(undefined);
   });
 
   test('appEntryCallback - Type LoginTokenOffline', async () => {
     // Arrange
-    jest.spyOn(UserFunctions, 'getUser').mockResolvedValueOnce(sampleUser);
     jest
       .spyOn(SystemFunctions, 'isFirstAppEntryToday')
       .mockResolvedValueOnce(true);
@@ -149,7 +101,6 @@ describe('SystemEvents Functions Tests', () => {
     );
 
     // Assert
-    expect(UserFunctions.getUser).toHaveBeenCalledTimes(1);
     expect(
       ClientSessionEventFunctions.handleClientSessionEvent,
     ).toHaveBeenCalledTimes(1);
@@ -159,14 +110,12 @@ describe('SystemEvents Functions Tests', () => {
     expect(SystemFunctions.isFirstAppEntryToday).toHaveBeenCalledTimes(1);
     expect(StreakFunctions.checkStreakBreak).toHaveBeenCalledTimes(1);
     expect(SyncProcessFunctions.runSyncProcess).toHaveBeenCalledTimes(0);
-    expect(DeviceFunctions.retrieveOrRegisterDeviceId).toHaveBeenCalledTimes(0);
     expect(StreakEvents.registerStreakNotifcation).toHaveBeenCalledTimes(1);
     expect(response).toEqual(undefined);
   });
 
   test('appEntryCallback - Type LoginAuthenticated', async () => {
     // Arrange
-    jest.spyOn(UserFunctions, 'getUser').mockResolvedValueOnce(sampleUser);
     jest
       .spyOn(SystemFunctions, 'isFirstAppEntryToday')
       .mockResolvedValueOnce(true);
@@ -177,7 +126,6 @@ describe('SystemEvents Functions Tests', () => {
     );
 
     // Assert
-    expect(UserFunctions.getUser).toHaveBeenCalledTimes(1);
     expect(
       ClientSessionEventFunctions.handleClientSessionEvent,
     ).toHaveBeenCalledTimes(2);
@@ -190,14 +138,12 @@ describe('SystemEvents Functions Tests', () => {
     expect(SystemFunctions.isFirstAppEntryToday).toHaveBeenCalledTimes(1);
     expect(StreakFunctions.checkStreakBreak).toHaveBeenCalledTimes(1);
     expect(SyncProcessFunctions.runSyncProcess).toHaveBeenCalledTimes(1);
-    expect(DeviceFunctions.retrieveOrRegisterDeviceId).toHaveBeenCalledTimes(1);
     expect(StreakEvents.registerStreakNotifcation).toHaveBeenCalledTimes(1);
     expect(response).toEqual(undefined);
   });
 
   test('appEntryCallback - Type CreateAccAuthed', async () => {
     // Arrange
-    jest.spyOn(UserFunctions, 'getUser').mockResolvedValueOnce(sampleUser);
     jest
       .spyOn(SystemFunctions, 'isFirstAppEntryToday')
       .mockResolvedValueOnce(true);
@@ -208,7 +154,6 @@ describe('SystemEvents Functions Tests', () => {
     );
 
     // Assert
-    expect(UserFunctions.getUser).toHaveBeenCalledTimes(1);
     expect(
       ClientSessionEventFunctions.handleClientSessionEvent,
     ).toHaveBeenCalledTimes(3);
@@ -224,7 +169,6 @@ describe('SystemEvents Functions Tests', () => {
     expect(SystemFunctions.isFirstAppEntryToday).toHaveBeenCalledTimes(1);
     expect(StreakFunctions.checkStreakBreak).toHaveBeenCalledTimes(1);
     expect(SyncProcessFunctions.runSyncProcess).toHaveBeenCalledTimes(1);
-    expect(DeviceFunctions.retrieveOrRegisterDeviceId).toHaveBeenCalledTimes(1);
     expect(StreakEvents.registerStreakNotifcation).toHaveBeenCalledTimes(1);
     expect(response).toEqual(undefined);
   });
